f4c88d710e9b91c14d7ee814a5513986e7bc17d3
Patricia <patricia.femenias@gmail.com>
2023-11-20 21:05:08 +0100

    Add UserStructure and UserCredentialsStructure

---
+++ src/features/user/types.ts
@@ -0,0 +1,14 @@
+import { type Request } from "express";
+
+export interface UserStructure {
+  _id: string;
+  name: string;
+  password: string;
+  username: string;
+}
+
+export type UserCredentialsStructure = Request<
+  Record<string, unknown>,
+  Record<string, unknown>,
+  { username: string; password: string }
+>;
